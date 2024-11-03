ffd69d8e7a621b17677a3357dda5ae71ac4a1ef7
FungAu <196588706@qq.com>
2024-11-03 21:25:39 +0800

    feat: 🎸 修改路由模式为哈希路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: () => import("@/pages/MainView.vue") },
@@ -8,6 +8,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
